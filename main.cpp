c7cd4033c68ab4c8f4af9edecdf0c5912d4a2259
Omri <omri1324@gmail.com>
2025-04-16 23:49:31 +0300

    Added app icon

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/app_icon"));
+
     MainWindow w;
     w.show();
     return a.exec();
